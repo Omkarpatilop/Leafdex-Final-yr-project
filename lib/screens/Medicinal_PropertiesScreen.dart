85dae19dde75e7932d30a3db76dac464f59e9096
MufcVarun11 <nairvarun2607@gmail.com>
2023-04-22 19:32:01 +0530

    final commit

--- lib/screens/Medicinal_PropertiesScreen.dart
+++ lib/screens/Medicinal_PropertiesScreen.dart
@@ -8,49 +8,109 @@ import 'package:nebat/services/apis.dart';
 class MedicinalPropertiesScreen extends StatelessWidget {
   final String plantName;
 
-  const MedicinalPropertiesScreen({Key? key,  required this.plantName})
+  const MedicinalPropertiesScreen({Key? key, required this.plantName})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: kPrimaryColor,
+    return Container(
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage("assets/images/bg.jpg"),
+          fit: BoxFit.cover,
+        ),
       ),
-      body: SafeArea(
-      left: true,
-      right: true,
-      minimum: EdgeInsets.all(10),
-        
-        child: FutureBuilder<MedicinalProperties>(
-          future: PlantsAPI().getMedicinalProperties(plantName),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const Center(
-                child: CircularProgressIndicator(
-                  color: Colors.green,
-                  strokeWidth: 10,
-                ),
-              );
-            } else if (snapshot.hasError) {
-              return Text('Error: ${snapshot.error}');
-            } else {
-              final medicinalProperties = snapshot.data!;
-      
-              return Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text('Medicinal Properties of $plantName'),
-                  SizedBox(height: 20),
-                  Text('Property 1: ${medicinalProperties.commonName}'),
-                  SizedBox(height: 10),
-                  Text('Property 2: ${medicinalProperties.description}'),
-                  SizedBox(height: 10),
-                  Text('Property 3: ${medicinalProperties.medicinalProperties}'),
-                ],
-              );
-            }
-          },
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          backgroundColor: kPrimaryColor,
+        ),
+        body: SafeArea(
+          left: true,
+          right: true,
+          minimum: const EdgeInsets.all(10),
+          child: FutureBuilder<MedicinalProperties>(
+            future: PlantsAPI().getMedicinalProperties(plantName),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Center(
+                  child: CircularProgressIndicator(
+                    color: Colors.green,
+                    strokeWidth: 10,
+                  ),
+                );
+              } else if (snapshot.hasError) {
+                return Text('Error: ${snapshot.error}');
+              } else {
+                final medicinalProperties = snapshot.data!;
+
+                return Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Medicinal Properties of $plantName',
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                      ),
+                    ),
+                    const SizedBox(height: 28),
+                    const Text(
+                      'Common Name: ',
+                      style: TextStyle(
+                        fontFamily: 'Oswald',
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      '${medicinalProperties.commonName}',
+                      style: const TextStyle(
+                        fontFamily: 'Oswald',
+                        fontSize: 16,
+                        fontWeight: FontWeight.normal,
+                      ),
+                    ),
+                    const SizedBox(height: 28),
+                    const Text(
+                      'Description: ',
+                      style: TextStyle(
+                        fontFamily: 'Oswald',
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      '${medicinalProperties.description}',
+                      style: const TextStyle(
+                        fontFamily: 'Oswald',
+                        fontSize: 16,
+                        fontWeight: FontWeight.normal,
+                      ),
+                    ),
+                    const SizedBox(height: 28),
+                    const Text(
+                      'Medicinal Properties: ',
+                      style: TextStyle(
+                        fontFamily: 'Open sans',
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      '${medicinalProperties.medicinalProperties}',
+                      style: const TextStyle(
+                        fontFamily: 'Open sans',
+                        fontSize: 16,
+                        fontWeight: FontWeight.normal,
+                      ),
+                    ),
+                  ],
+                );
+              }
+            },
+          ),
         ),
       ),
     );
